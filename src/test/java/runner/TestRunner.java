f9f87707920d85d161da77c35c3524852c98d5a7
Maya Babuji <mayababuji@outlook.com>
2026-03-23 11:06:20 -0400

    changes for chaintest and extent reports

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,9 +4,13 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
+
+
 @CucumberOptions(plugin={
         "pretty","html:target/SeleniumHackathon.html", "json:target/cucumber.json",
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        "com.aventstack.chaintest.plugins.ChainTestCucumberListener:"
 
 
 },
