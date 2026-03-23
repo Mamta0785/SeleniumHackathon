1d7af07d8ee09692cf5eaf048a37cf7cdc722264
nehakadam1025 <neha.kadam@gmail.com>
2026-03-23 10:39:09 -0500

    Feature file for View Test Reports

---
+++ src/test/java/stepdefinition/ViewTestReportStepDefinition.java
@@ -0,0 +1,130 @@
+package stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ViewTestReportStepDefinition {
+	
+	@Given("User is logged into the app")
+	public void user_is_logged_into_the_app() {
+	    
+	}
+
+	@Given("patients already exist in the system")
+	public void patients_already_exist_in_the_system() {
+	    
+	}
+
+	@Given("User is on the My Patients page")
+	public void user_is_on_the_my_patients_page() {
+	    
+	}
+
+	@When("User clicks the View Previous Test Reports button for a specific record")
+	public void user_clicks_the_view_previous_test_reports_button_for_a_specific_record() {
+	    
+	}
+
+	@Then("the corresponding report for that record should be opened")
+	public void the_corresponding_report_for_that_record_should_be_opened() {
+	    
+	}
+
+	@Then("the title {string} should be displayed")
+	public void the_title_should_be_displayed(String string) {
+	    
+	}
+
+	@Then("the Close icon {string} should be displayed")
+	public void the_close_icon_should_be_displayed(String string) {
+	    
+	}
+
+	@Then("the Patient Id corresponding to the selected record should be displayed")
+	public void the_patient_id_corresponding_to_the_selected_record_should_be_displayed() {
+	   
+	}
+
+	@Then("the Patient Name corresponding to the selected record should be displayed")
+	public void the_patient_name_corresponding_to_the_selected_record_should_be_displayed() {
+	    
+	}
+
+	@Then("the Patient Email corresponding to the selected record should be displayed")
+	public void the_patient_email_corresponding_to_the_selected_record_should_be_displayed() {
+	   
+	}
+
+	@Then("the Contact Number corresponding to the selected record should be displayed")
+	public void the_contact_number_corresponding_to_the_selected_record_should_be_displayed() {
+	    
+	}
+
+	@Then("the Reports table should be displayed")
+	public void the_reports_table_should_be_displayed() {
+	    
+	}
+
+	@Then("the pagination controls First \\(<<), Previous \\(<), Next \\(>), Last \\(>>) arrows should be displayed")
+	public void the_pagination_controls_first_previous_next_last_arrows_should_be_displayed() {
+	    
+	}
+
+	@Then("the table header {string} should be displayed")
+	public void the_table_header_should_be_displayed(String string) {
+	   
+	}
+
+	@Then("the headers should appear in the exact order: Record Number → File → Uploaded Time → File\\/Report Name → Vitals → Identified Health Conditions")
+	public void the_headers_should_appear_in_the_exact_order_record_number_file_uploaded_time_file_report_name_vitals_identified_health_conditions() {
+	   
+	}
+
+	@Then("each report row should display a Record Number")
+	public void each_report_row_should_display_a_record_number() {
+	   
+	}
+
+	@Then("each report row should display a View PDF button")
+	public void each_report_row_should_display_a_view_pdf_button() {
+	    
+	}
+
+	@Then("each report row should display the Uploaded Time")
+	public void each_report_row_should_display_the_uploaded_time() {
+	    
+	}
+
+	@Then("each report row should display the File\\/Report Name")
+	public void each_report_row_should_display_the_file_report_name() {
+	    
+	}
+
+	@Then("each report row should display Vitals information")
+	public void each_report_row_should_display_vitals_information() {
+	    
+	}
+
+	@Then("each report row should display Identified Health Conditions")
+	public void each_report_row_should_display_identified_health_conditions() {
+	    
+	}
+
+	@Then("Vitals should be displayed in the order: Weight → Height → Temperature → SP → DP")
+	public void vitals_should_be_displayed_in_the_order_weight_height_temperature_sp_dp() {
+	    
+	}
+
+	@Then("Vitals should be displayed in multiline format")
+	public void vitals_should_be_displayed_in_multiline_format() {
+	   
+	}
+
+	@Then("Identified Health Conditions should be displayed in multiline format")
+	public void identified_health_conditions_should_be_displayed_in_multiline_format() {
+	   
+	}
+
+
+}
